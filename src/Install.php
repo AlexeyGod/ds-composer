088a77fb0b11d8fe7f54e75dff13db3e077e9bab
AlexeyGod <forlem@gmail.com>
2020-07-24 00:48:07 +0300

    refactoring code

--- src/Install.php
+++ src/Install.php
@@ -4,7 +4,7 @@ namespace framework\composer;
 
 class Installer {
 
-	public static function postInstallProject()
+	public static function postInstallProject($event)
 	{
 		$dir = dirname(dirname(dirname(__DIR__))).'/assets';
 		mkdir($dir);
