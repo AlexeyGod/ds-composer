78b1a9b959dde8c6f93e1cc219f2c58dd0a51872
AlexeyGod <forlem@gmail.com>
2020-07-23 21:03:52 +0300

    base version

---
+++ Install.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace framework\\composer;
+
+class Installer {
+
+	public static function postInstallProject()
+	{
+		$dir = dirname(dirname(dirname(__DIR__))).'/assets';
+		mkdir($dir);
+		chmod($dir, 0777);
+	}
+
+}
+?>
