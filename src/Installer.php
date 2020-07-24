7deacca1897d3f03aef93db75c764f637d69c15f
AlexeyGod <forlem@gmail.com>
2020-07-24 14:35:21 +0300

    refactoring code

--- src/Installer.php
+++ src/Installer.php
@@ -6,7 +6,7 @@ class Installer {
 
 	public static function postInstallProject($event)
 	{
-		$dir = dirname(dirname(dirname(__DIR__))).'/assets';
+		$dir = dirname(dirname(__DIR__)).'/assets';
 		mkdir($dir);
 		chmod($dir, 0777);
 	}
