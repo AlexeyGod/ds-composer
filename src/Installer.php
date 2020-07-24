39ad590d85436c072984384e790e08687621bf7c
AlexeyGod <forlem@gmail.com>
2020-07-24 16:50:26 +0300

    refactoring code

--- src/Installer.php
+++ src/Installer.php
@@ -6,9 +6,16 @@ class Installer {
 
 	public static function postInstallProject($event)
 	{
-		$dir = dirname(dirname(__DIR__)).'/assets';
+		// Current directory vendor/alexeygod/composer
+		$dir = dirname( // up to alexeygod
+			dirname( // up to vendor
+				__DIR__
+			))
+		.'/assets';
+
 		mkdir($dir);
 		chmod($dir, 0777);
+		file_put_contents($dir.'/composer.info.txt', 'Variable $DIR: '.$dir."\r\n".'DIR: '.__DIR__);
 	}
 
 }
