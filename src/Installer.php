062f3392ae7effe6bd06598310f494facda90b32
AlexeyGod <forlem@gmail.com>
2020-07-24 17:11:47 +0300

    refactoring code

--- src/Installer.php
+++ src/Installer.php
@@ -7,23 +7,10 @@ class Installer {
 	public static function postInstallProject($event)
 	{
 		// Current directory \vendor\alexeygod\ds-composer\src
-		$dir1 = dirname(__DIR__); //Up to ds-composer
-		$dir2 = dirname($dir1); // up to alexeygod
-		$dir3 = dirname($dir2); // up to vendor
-						
-		$dir = $dir3.'/assets';
+		$dir = dirname(dirname(dirname(dirname(__DIR__)))).'/assets';
 
 		mkdir($dir);
 		chmod($dir, 0777);
-		file_put_contents($dir.'/composer.info.txt', 'Variable $DIR: '.$dir
-			."\r\n"
-			."DIR1: ".$dir1
-			."\r\n"
-			."DIR2: ".$dir2
-			."\r\n"
-			."DIR3: ".$dir3
-			."\r\n"
-			.'DIR: '.__DIR__);
 	}
 
 }
